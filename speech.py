bb4467ef09b6af904e712a12ffc59b26619b2cf3
Anntey <51882759+Anntey@users.noreply.github.com>
2019-08-14 10:15:11 +0300

    Update speech.py

--- speech.py
+++ speech.py
@@ -6,9 +6,10 @@ import re
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from glob import glob
+from re import match
 from scipy.io import wavfile
 from scipy.signal import spectrogram
-from glob import glob
 from keras.models import Model
 from keras.optimizers import Adam, SGD
 from keras.callbacks import CSVLogger
@@ -30,46 +31,27 @@ test_path = "./input/test/test/audio/"
 # Utility functions #
 #####################
 
-def log_spectrogram(audio, sample_rate = 16000, window_size = 20, step_size = 10, eps = 1e-10):
+def log_spectrogram(audio, sample_rate = 16000, window_size = 20, step_size = 10, eps = 1e-10): # why transpose ??????????????????
     nperseg = int(round(window_size * sample_rate / 1e3))
     noverlap = int(round(step_size * sample_rate / 1e3))
     freqs, times, spec = spectrogram(audio, fs = sample_rate, window = "hann", nperseg = nperseg, noverlap = noverlap, detrend = False)
     return freqs, times, np.log(spec.T.astype(np.float32) + eps)
 
-def get_labels_filenames(dirpath, extension = "wav"):
-    filepaths = glob(dirpath + r"*/*" + extension)
-    
-    pattern = r".+/(\w+)/\w+\." + extension + "$" # regex the label
-    labels = []
-    for filepath in filepaths:
-        label = re.match(pattern, filepath)
-        if label:
-            labels.append(label.group(1))
-            
-    pattern = r".+/(\w+\." + extension + ")$" # regex the filename
-    filenames = []
-    for filepath in filepaths:
-        filename = re.match(pattern, filepath)
-        if filename:
-            filenames.append(filename.group(1))
-            
-    return labels, filenames
-
-def pad_audio(samples): # pad audios less than 16000 (1 second) with 0s
+def pad_audio(samples): # pad audios less than 16000 with 0s
     if len(samples) >= sample_rate:       
         return samples
     else:
         return np.pad(samples, pad_width = (sample_rate - len(samples), 0), mode = "constant", constant_values = (0, 0))
 
-def chop_audio(samples, sample_rate = 16000, num = 10000): # chop audios larger than 16000 (background noise wavs) to 16000 in length and create chunks out of one large wav files
-    for i in range(num):
+def chop_audio(samples, sample_rate = 16000, n = 10000): # chop audios larger than 16000 (background noise wavs) to 16000
+    for i in range(n):
         beg = np.random.randint(0, len(samples) - sample_rate)
         yield samples[beg: beg + sample_rate]
 
-def encode_labels(labels): # transform labels into dummies
+def encode_labels(orig_labels): # transform labels into dummies
     new_labels = []
     
-    for label in labels:
+    for label in orig_labels:
         if label == "_background_noise_":
             new_labels.append("silence")
         elif label not in labels_list:
@@ -83,27 +65,43 @@ def encode_labels(labels): # transform labels into dummies
 # Prepare training data #
 #########################
 
-labels_train, filenames_train = get_labels_filenames(train_path)
+filepaths_train = glob(train_path + r"*/*" + "wav")
+  
+labels_train = []
+pattern_label = r".+/(\w+)/\w+\." + extension + "$" # regex label
 
+for filepath in filepaths_train:
+    label = match(pattern_label, filepath)
+    if label:
+        labels_train.append(label.group(1)) # append capture group
+
+filenames_train = []            
+pattern_filename = r".+/(\w+\." + "wav" + ")$" # regex filename
+
+for filepath in filepaths_train:
+    filename = match(pattern_filename, filepath)
+    if filename:
+        filenames_train.append(filename.group(1)) # append capture group
+            
 y_train = []
 x_train = []
 
 for label, filename in zip(labels_train, filenames_train):
-    sample_rate, samples = wavfile.read(train_path + "/" + label + "/" + filename)
-    samples = pad_audio(samples)
+    _, sample = wavfile.read(train_path + label + "/" + filename) # "/" + sample_rate
+    sample = pad_audio(samples)
     
-    if len(samples) > 16000:
+    if len(sample) > 16000:
         n_samples = chop_audio(samples)
     else:
-        n_samples = [samples]
+        n_samples = [sample] # list
         
-    for samples in n_samples:
-        _, _, specgram = log_spectrogram(samples)
+    for sample in n_samples:
+        _, _, specgram = log_spectrogram(sample)
         y_train.append(label)
         x_train.append(specgram)
         
 x_train = np.array(x_train)
-x_train = np.expand_dims(x_train, axis = 3)
+x_train = np.expand_dims(x_train, axis = 3) # reshape to (n, 99, 161, 1)
 
 y_train = encode_labels(y_train) 
 label_index = y_train.columns.values # used by pandas to create dummy values, we need it for later use.
@@ -113,11 +111,14 @@ y_train = np.array(y_train.values)
 # Visualization #
 #################
 
+import random
+indices, locus = random.sample(range(0, len(x_train.shape[0])), 9), range(0, 9)
+
 plt.figure(figsize = (10, 10))
-for i in range(9, 18):
-    plt.subplot(3, 3, i - 9 + 1)
+for i, locus in zip(indices, locus):
+    plt.subplot(3, 3, locus)
     plt.title(labels_train[i])    
-    plt.imshow(x_train[i].squeeze().T, aspect = "auto", origin = "lower")
+    plt.imshow(x_train[i].squeeze().T, aspect = "auto", origin = "lower") 
     plt.axis("off")
 
 #################
@@ -127,30 +128,30 @@ for i in range(9, 18):
 inp = Input(shape = (99, 161, 1))
 
 x = BatchNormalization()(inp)
-x = Conv2D(8, kernel_size = (2, 2), activation = "relu")(x)
-x = Conv2D(8, kernel_size = (2, 2), activation = "relu")(x)
+x = Conv2D(8, (2, 2), activation = "relu")(x)
+x = Conv2D(8, (2, 2), activation = "relu")(x)
 x = BatchNormalization()(x)
-x = MaxPooling2D(pool_size = (2, 2))(x)
-x = Dropout(rate = 0.2)(x)
-x = Conv2D(16, kernel_size = (3, 3), activation = "relu")(x)
-x = Conv2D(16, kernel_size = (3, 3), activation = "relu")(x)
+x = MaxPooling2D((2, 2))(x)
+x = Dropout(0.2)(x)
+x = Conv2D(16, (3, 3), activation = "relu")(x)
+x = Conv2D(16, (3, 3), activation = "relu")(x)
 x = BatchNormalization()(x)
-x = MaxPooling2D(pool_size = (2, 2))(x)
-x = Dropout(rate = 0.2)(x)
-x = Conv2D(32, kernel_size = (3, 3), activation = "relu")(x)
-x = Conv2D(32, kernel_size = (3, 3), activation = "relu")(x)
+x = MaxPooling2D((2, 2))(x)
+x = Dropout(0.2)(x)
+x = Conv2D(32, (3, 3), activation = "relu")(x)
+x = Conv2D(32, (3, 3), activation = "relu")(x)
 x = BatchNormalization()(x)
-x = MaxPooling2D(pool_size = (2, 2))(x)
-x = Dropout(rate = 0.2)(x)
-x = Conv2D(64, kernel_size = (3, 3), activation = "relu")(x)
-x = Conv2D(64, kernel_size = (3, 3), activation = "relu")(x)
+x = MaxPooling2D((2, 2))(x)
+x = Dropout(0.2)(x)
+x = Conv2D(64, (3, 3), activation = "relu")(x)
+x = Conv2D(64, (3, 3), activation = "relu")(x)
 x = BatchNormalization()(x)
-x = MaxPooling2D(pool_size = (2, 2))(x)
+x = MaxPooling2D((2, 2))(x)
 x = Flatten()(x)
 x = Dropout(0.5)(x)
 x = Dense(512, activation = "relu")(x)
 x = BatchNormalization()(x)
-x = Dropout(rate = 0.5)(x)
+x = Dropout(0.5)(x)
 x = Dense(256, activation = "relu")(x)
 x = BatchNormalization()(x)
 outp = Dense(12, activation = "softmax")(x)
@@ -158,9 +159,9 @@ outp = Dense(12, activation = "softmax")(x)
 model = Model(inp, outp)
 
 model.compile(
-        loss = "binary_crossentropy",
+        loss = "binary_crossentropy", # categorical ????????????????
         optimizer = Adam(lr = 1e-3),
-#        optimizer = SGD(lr = 0.01, decay = 1e-4, momentum = 0.9, nesterov = True),
+#       optimizer = SGD(lr = 1e-3, decay = 1e-4, momentum = 0.9, nesterov = True),
         metrics = ["accuracy"]
 )
 
@@ -182,8 +183,8 @@ callbacks_list = [
 fit_log = model.fit(
         x_train,
         y_train,
-        batch_size = 64,
         validation_data = (x_val, y_val),
+        batch_size = 64,
         epochs  = 9,
         shuffle = True,
         callbacks = callbacks_list
@@ -220,10 +221,10 @@ def test_gen(batch_size):
         imgs.append(specgram)
         filenames.append(path.split("/")[-1]) # split path and get filename
         
-        if i == batch_size: # stop at batch size
+        if i == batch_size: # continue until batch size
             i = 0
             imgs = np.array(imgs)
-            imgs = np.expand_dims(imgs, axis = 3)
+            imgs = np.expand_dims(imgs, axis = 3) #2???????
             yield filenames, imgs
             
     if i < batch_size: # leftover batch
@@ -236,7 +237,7 @@ def test_gen(batch_size):
 filenames = []
 labels = []
 
-for name, img in test_gen(batch_size = 64):
+for name, img in test_gen(batch_size = 64): # miten toimii???????
     pred = model.predict(img)
     pred = np.argmax(pred, axis = 1)
     pred = list(label_index[i] for i in pred)
